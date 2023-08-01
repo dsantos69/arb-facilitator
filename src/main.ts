6a041f135e94c40213abf1967590e45af548eb8f
DSantos69 <david.csantos69@gmail.com>
2023-08-01 18:06:19 -0300

    added initial express configuration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-function main() {}
+import express, { Express, Router, Request, Response } from 'express';
 
-main();
+const app: Express = express();
+
+const route = Router();
+
+app.use(express.json());
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello, World' });
+});
+
+app.use(route);
+
+app.listen(3333, () => {
+  console.log('⚡️[server]: server running at http://localhost:3333}');
+});
